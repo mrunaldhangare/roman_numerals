00688ecdd7b5decc8aefdaba8adbb2d3654ad476
mrunaldhangare <mrunalyetekar@gmail.com>
2023-02-07 00:39:15 +0000

    implemented convertNumberToRoman with test cases & updated tests for convertRomanToNumber

--- roman-numerals.js
+++ roman-numerals.js
@@ -9,23 +9,55 @@ const ROMAN_NUMBER_MAP = {
 }
 
 const convertRomanToNumber = (romanNumeral) => {
+    if (!romanNumeral) throw new Error("Invalid input!");
 
     const romanNumerals = romanNumeral.split("");
 
-    const result = romanNumerals.reduce((numberValue, romanVal, currentIndex) => {
+    const result = romanNumerals.reduce((numVal, romanVal, currentIndex) => {
+
         const currentVal = ROMAN_NUMBER_MAP[romanVal];
         const nextVal = ROMAN_NUMBER_MAP[romanNumerals[currentIndex + 1]];
 
         if (currentVal < nextVal) {
-            numberValue -= currentVal;
+            numVal -= currentVal;
         } else {
-            numberValue += currentVal;
+            numVal += currentVal;
         }
 
-        return numberValue;
+        return numVal;
     }, 0)
 
     return result;
 }
 
-module.exports = convertRomanToNumber;
+const NUMBER_ROMAN_MAP = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100: "C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M"
+}
+
+const convertNumberToRoman = (number) => {
+    if (!number) throw new Error("Invalid input!");
+    const numberKeys = Object.keys(NUMBER_ROMAN_MAP).reverse();
+
+    const romanNumeral = numberKeys.reduce((romanVal, numKey) => {
+        const prefixRomanValue = Math.floor(number / numKey);
+        number -= numKey * prefixRomanValue;
+        romanVal += NUMBER_ROMAN_MAP[numKey].repeat(prefixRomanValue);
+        return romanVal;
+    }, "")
+
+    return romanNumeral;
+}
+
+module.exports = { convertRomanToNumber, convertNumberToRoman };
--- roman-numerals.test.js
+++ roman-numerals.test.js
@@ -1,24 +1,69 @@
-const convertRomanToNumber = require('./roman-numerals');
+const { convertRomanToNumber, convertNumberToRoman } = require('./roman-numerals');
 
-test("takes roman numerals and returns number", () => {
-    expect(convertRomanToNumber("I")).toBe(1);
 
-});
-test("takes roman numerals and returns number by accessing from mapping", () => {
-    expect(convertRomanToNumber("V")).toBe(5);
+describe("convertRomanToNumber", () => {
 
-});
-test("takes roman numerals and returns number by using array", () => {
-    expect(convertRomanToNumber("II")).toBe(2);
-    expect(convertRomanToNumber("III")).toBe(3);
+    test("returns error when input is undefined", () => {
+        const results = () => convertRomanToNumber();
+        expect(results).toThrow(new Error("Invalid input!"));
+    });
+    test("returns error when input is empty", () => {
+        const results = () => convertRomanToNumber('');
+        expect(results).toThrow(new Error("Invalid input!"));
+    });
 
+    test("takes roman numerals and returns number", () => {
+        expect(convertRomanToNumber("I")).toBe(1);
+
+    });
+    test("takes roman numerals and returns number by accessing from mapping", () => {
+        expect(convertRomanToNumber("V")).toBe(5);
+
+    });
+    test("takes roman numerals and returns number by using array", () => {
+        expect(convertRomanToNumber("II")).toBe(2);
+        expect(convertRomanToNumber("III")).toBe(3);
+
+    });
+    test("takes roman numerals and returns number by subtracting current value from next value", () => {
+        expect(convertRomanToNumber("IV")).toBe(4);
+    });
+    test("takes any roman numeral and returns correct number", () => {
+        expect(convertRomanToNumber("XXXI")).toBe(31);
+        expect(convertRomanToNumber("CCCLXIX")).toBe(369);
+        expect(convertRomanToNumber("LXXX")).toBe(80);
+        expect(convertRomanToNumber("MMDCCLI")).toBe(2751);
+    });
 });
-test("takes roman numerals and returns number by subtracting current value from next value", () => {
-    expect(convertRomanToNumber("IV")).toBe(4);
-});
-test("takes any roman numeral and returns correct number", () => {
-    expect(convertRomanToNumber("XXXI")).toBe(31);
-    expect(convertRomanToNumber("CCCLXIX")).toBe(369);
-    expect(convertRomanToNumber("LXXX")).toBe(80);
-    expect(convertRomanToNumber("MMDCCLI")).toBe(2751);
+
+
+describe("convertNumberToRoman", () => {
+
+    test("returns error when input is 0", () => {
+        const results = () => convertNumberToRoman(0);
+        expect(results).toThrow(new Error("Invalid input!"));
+    });
+    test("returns error when input is undefined", () => {
+        const results = () => convertNumberToRoman();
+        expect(results).toThrow(new Error("Invalid input!"));
+    });
+    test("returns error when input is empty", () => {
+        const results = () => convertNumberToRoman('');
+        expect(results).toThrow(new Error("Invalid input!"));
+    });
+    test("takes number and returns roman numeral", () => {
+        expect(convertNumberToRoman(1)).toBe("I");
+        expect(convertNumberToRoman(5)).toBe("V");
+        expect(convertNumberToRoman(50)).toBe("L");
+    });
+    test("takes number and returns roman numeral by repeating similar roman numerals", () => {
+        expect(convertNumberToRoman(2)).toBe("II");
+        expect(convertNumberToRoman(20)).toBe("XX");
+    });
+    test("takes any number and returns correct roman numeral", () => {
+        expect(convertNumberToRoman(2751)).toBe("MMDCCLI");
+        expect(convertNumberToRoman(369)).toBe("CCCLXIX");
+        expect(convertNumberToRoman(31)).toBe("XXXI");
+    });
+
 });
